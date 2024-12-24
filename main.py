7bb49af7fb4722c143c662d0f26b895c1a7ed056
Masenseii <brunoshedy@gmail.com>
2024-12-24 13:26:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -408,7 +408,7 @@ elif app_mode == 'Disease Recognition':
         st.info("Please ensure your browser allows camera access to use this feature.")
 
         camera_image = st.camera_input("Take a picture")
-        
+
       if camera_image is not None:
             image_name = camera_image.name  # Access the name only if the file is there
             st.image(camera_image, caption="Taken Photo", use_container_width=True)
